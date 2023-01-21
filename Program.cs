2c7370ddd08cdb05146df9d6105cef4888fb21d5
aaki1909 <121802239+aaki1909@users.noreply.github.com>
2023-01-21 17:28:01 +0530

    c sharp assignment

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number :   ");  
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number :   ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Addition of two numbers : {0}" , x + y);
+            Console.WriteLine("Subtraction of two numbers : {0}  " , x - y);
+            Console.WriteLine("Multiplication of two numbers : {0}   " , x * y);
+            Console.WriteLine("Division of two numbers :  {0} " , x / y);
+
+        }
+    }
+}
